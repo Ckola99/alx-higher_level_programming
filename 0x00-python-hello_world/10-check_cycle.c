445ca3b6cda905fa24e34defb79d0fce3c502cda
CKola <christopherkola@gmail.com>
2023-06-06 09:13:29 +0000

    mandatory and advanced tasks

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,17 +9,20 @@
  *         If there is a cycle - 1.
  */
 
-int check_cycle(listint_t *list) {
-    listint_t *slow = list;
-    listint_t *fast = list;
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-    while (fast != NULL && fast->next != NULL) {
-        slow = slow->next;
-        fast = fast->next->next;
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
 
-        if (slow == fast) {
-            return (1);  // Cycle detected
-        }
-    }
-    return (0);  // No cycle detected
+		if (slow == fast)
+		{
+			return (1);
+		}
+	}
+	return (0); 
 }
