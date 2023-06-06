81048a4821bc536753690a2b9c31927ff1a513ee
CKola <christopherkola@gmail.com>
2023-06-06 12:56:28 +0000

    corrections

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number == 99:
-        print("{}".format(number))
-    else:
-        print("{:02}".format(number), end=", ")
+for num1 in range(10):
+    for num2 in range(num1 + 1, 10):
+        if num1 == 8 and num2 == 9:
+            print("{}{}".format(num1, num2))
+        else:
+            print("{}{}".format(num1, num2), end=", ")
